93c1f08e5039c6332fd9805e843d347633ce9366
Gravitana <v.a.grahov@yandex.ru>
2024-03-17 20:21:24 +0500

    код семинара

--- src/main/java/ru/gravitana/tic_tac_toe/Map.java
+++ src/main/java/ru/gravitana/tic_tac_toe/Map.java
@@ -4,58 +4,98 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Map extends JPanel {
     private static final Random RANDOM = new Random();
     private static final int PADDING = 5;
 
-    private int gameOverType;
-    private static final int STATE_DRAW = 0;
+    private final int EMPTY_DOT = 0;
+    private final int HUMAN_DOT = 1;
+    private final int AI_DOT = 2;
+
+    private int gameStateType;
+    private static final int STATE_GAME = 0;
     private static final int STATE_WIN_HUMAN = 1;
     private static final int STATE_WIN_AI = 2;
+    private static final int STATE_DRAW = 3;
 
     private static final String MSG_WIN_HUMAN = "Победил игрок!";
     private static final String MSG_WIN_AI = "Победил компьютер!";
     private static final String MSG_DRAW = "Ничья!";
 
-    private boolean isGameOver;
-    private boolean isInitialized;
+    private int width, height, cellWidth, cellHeight;
+    private int mode, fieldSizeX, fieldSizeY, winLen;
+    private int[][] field;
+    private boolean gameWork;
 
-    private final int EMPTY_DOT = 0;
-    private final int HUMAN_DOT = 1;
-    private final int AI_DOT = 2;
-    private  int fieldSizeX = 3;
-    private  int fieldSizeY = 3;
-    private char[][] field;
 
-    private int panelWidth;
-    private int panelHeight;
-    private int cellWidth;
-    private int cellHeight;
+
+
+
+//    private int gameOverType;
+//
+//    private boolean isGameOver;
+//    private boolean isInitialized;
+//    private  int fieldSizeX = 3;
+//    private  int fieldSizeY = 3;
+//    private int[][] field;
+//
+//    private int panelWidth;
+//    private int panelHeight;
+//    private int cellWidth;
+//    private int cellHeight;
 
     Map() {
         setBackground(Color.WHITE);
-
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                super.mouseReleased(e);
-                update(e);
+                if (gameWork) {
+                    update(e);
+                }
             }
         });
-
-        isInitialized = false;
     }
+
     private void initMap() {
-        fieldSizeX = 3;
-        fieldSizeY = 3;
-        field = new char[fieldSizeY][fieldSizeX];
-        for (int i = 0; i < fieldSizeY; i++) {
-            for (int j = 0; j < fieldSizeX; j++) {
-                field[i][j] = EMPTY_DOT;
-            }
+        field = new int[fieldSizeY][fieldSizeX];
+    }
+
+    void startNewGame(int mode, int sizeX, int sizeY, int winLen) {
+        this.mode = mode;
+        this.fieldSizeX = sizeX;
+        this.fieldSizeY = sizeY;
+        this.winLen = winLen;
+
+        initMap();
+        gameWork = true;
+        gameStateType = STATE_GAME;
+
+        repaint();
+    }
+
+    private void update(MouseEvent mouseEvent) {
+        int x = mouseEvent.getX() / cellWidth;
+        int y = mouseEvent.getY() / cellHeight;
+        if (!isValidCell(x, y) || !isEmptyCell(x, y)) {
+            return;
         }
+        field[y][x] = HUMAN_DOT;
+        if (checkEndGame(HUMAN_DOT, STATE_WIN_HUMAN)) {
+            return;
+        }
+        aiTurn();
+        repaint();
+        checkEndGame(AI_DOT, STATE_WIN_AI);
+    }
+
+    private void testBoard(){
+        for (int i = 0; i < 3; i++) {
+            System.out.println(Arrays.toString(field[i]));
+        }
+        System.out.println();
     }
 
     private boolean isValidCell(int x, int y) {
@@ -75,134 +115,115 @@ public class Map extends JPanel {
         field[y][x] = AI_DOT;
     }
 
-    private boolean checkWin(int c) {
-        if (field[0][0] == c && field[0][1] == c&& field[0][2] == c) return true;
-        if (field[1][0] == c && field[1][1] == c&& field[1][2] == c) return true;
-        if (field[2][0] == c && field[2][1] == c&& field[2][2] == c) return true;
-
-        if (field[0][0] == c && field[1][0] == c&& field[2][0] == c) return true;
-        if (field[0][1] == c && field[1][1] == c&& field[2][1] == c) return true;
-        if (field[0][2] == c && field[1][2] == c&& field[2][2] == c) return true;
-
-        if (field[0][0] == c && field[1][1] == c&& field[2][2] == c) return true;
-        if (field[0][2] == c && field[1][1] == c&& field[2][0] == c) return true;
-        return false;
-    }
-
     private boolean isMapFull() {
         for (int i = 0; i < fieldSizeY; i++) {
             for (int j = 0; j < fieldSizeX; j++) {
-                if (field[i][j] == EMPTY_DOT) return false;
+                if (field[i][j] == EMPTY_DOT) {
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    private void update(MouseEvent e) {
-        if (isGameOver || !isInitialized) return;
-        int cellX = e.getX()/cellWidth;
-        int cellY = e.getY()/cellHeight;
-        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)) return;
-        field[cellY][cellX] = HUMAN_DOT;
-        repaint();
-
-        if (checkEndGame(HUMAN_DOT, STATE_WIN_HUMAN)) {
-//            System.out.println("Победа человека");
-            return;
-        }
-        aiTurn();
-        repaint();
-        if (checkEndGame(AI_DOT, STATE_WIN_AI)) {
-//            System.out.println("Победа компа");
-            return;
-        }
-    }
-
     private boolean checkEndGame(int dot, int gameOverType) {
         if (checkWin(dot)) {
-            this.gameOverType = gameOverType;
-            isGameOver = true;
+            this.gameStateType = gameOverType;
             repaint();
             return true;
-        }
-        if (isMapFull()) {
-            this.gameOverType = STATE_DRAW;
-            isGameOver = true;
+        } else if (isMapFull()) {
+            this.gameStateType = STATE_DRAW;
             repaint();
             return true;
         }
         return false;
     }
 
-    void startNewGame(int mode, int fSzX, int fSzY, int winLen) {
-        System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d", mode, fSzX, fSzY, winLen);
+    private boolean checkWin(int dot){
+        for (int i = 0; i < fieldSizeX; i++) {
+            for (int j = 0; j < fieldSizeY; j++) {
+                if (checkLine(i, j, 1, 0, winLen, dot)) return true;
+                if (checkLine(i, j, 1, 1, winLen, dot)) return true;
+                if (checkLine(i, j, 0, 1, winLen, dot)) return true;
+                if (checkLine(i, j, 1, -1, winLen, dot)) return true;
+            }
+        }
+        return false;
+    }
 
-        initMap();
-        isGameOver = false;
-        isInitialized = true;
-        repaint();
+    private boolean checkLine(int x, int y, int vx, int vy, int len, int dot){
+        int far_x = x + (len - 1) * vx;
+        int far_y = y + (len - 1) * vy;
+        if (!isValidCell(far_x, far_y)){
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
+            if (field[y + i * vy][x + i * vx] != dot){
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        render(g);
+        if (gameWork) {
+            render(g);
+        }
     }
 
     private void render(Graphics g) {
-        if (!isInitialized) return;
-
-        panelWidth = getWidth();
-        panelHeight = getHeight();
-        cellWidth = panelWidth / 3;
-        cellHeight = panelHeight / 3;
+        width = getWidth();
+        height = getHeight();
+        cellWidth = width / fieldSizeX;
+        cellHeight = height / fieldSizeY;
 
         g.setColor(Color.BLACK);
-        for (int h = 0; h < 3; h++) {
+        for (int h = 0; h < fieldSizeX; h++) {
             int y = h * cellHeight;
-            g.drawLine(0, y, panelWidth, y);
+            g.drawLine(0, y, width, y);
         }
-        for (int w = 0; w < 3; w++) {
+        for (int w = 0; w < fieldSizeX; w++) {
             int x = w * cellWidth;
-            g.drawLine(x, 0, x, panelHeight);
+            g.drawLine(x, 0, x, height);
         }
 
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
-                if (field[y][x] == EMPTY_DOT) continue;
-
+                if (field[y][x] == EMPTY_DOT){
+                    continue;
+                }
                 if (field[y][x] == HUMAN_DOT) {
-                    g.setColor(Color.BLUE);
                     g.drawLine(x * cellWidth + PADDING, y * cellHeight + PADDING,
                             (x + 1) * cellWidth - PADDING, (y + 1) * cellHeight - PADDING);
                     g.drawLine(x * cellWidth + PADDING, (y + 1) * cellHeight - PADDING,
                             (x + 1) * cellWidth - PADDING, y * cellHeight + PADDING);
-                } else if (field[y][x] == AI_DOT){
-                    g.setColor(new Color(0xff0000));
+                } else if (field[y][x] == AI_DOT) {
                     g.drawOval(x * cellWidth + PADDING, y * cellHeight + PADDING,
                             cellWidth - PADDING * 2, cellHeight - PADDING * 2);
                 } else {
-                    throw new RuntimeException("Unexpected value " + field[y][x] +
+                    throw new RuntimeException("unchecked value " + field[y][x] +
                             " in cell: x=" + x + " y=" + y);
                 }
             }
         }
-
-        if (isGameOver) {
-            showMessageGameOver(g);
+        if (gameStateType != STATE_GAME){
+            showMessage(g);
         }
     }
 
-    private void showMessageGameOver(Graphics g) {
+    private void showMessage(Graphics g) {
         g.setColor(Color.DARK_GRAY);
-        g.clearRect(0, 200, getWidth(), 70);
-        g.setColor(Color.GREEN);
-        g.setFont(new Font("Times new roman", Font.BOLD, 48));
-        switch (gameOverType) {
-            case STATE_DRAW -> g.drawString(MSG_DRAW, 180, getHeight() / 2);
-            case STATE_WIN_AI -> g.drawString(MSG_WIN_AI, 20, getHeight() / 2);
-            case STATE_WIN_HUMAN -> g.drawString(MSG_WIN_HUMAN, 70, getHeight() / 2);
-            default -> throw new RuntimeException("Unexpected gameOver state: " + gameOverType);
+        g.fillRect(0, getHeight() / 2, getWidth(), 70);
+        g.setColor(Color.YELLOW);
+        g.setFont(new Font("Times new roman", Font.BOLD, 32));
+        switch (gameStateType){
+            case STATE_DRAW -> g.drawString(MSG_DRAW, 180, getHeight() / 2 + 60);
+            case STATE_WIN_HUMAN -> g.drawString(MSG_WIN_HUMAN, 20, getHeight() / 2 + 60);
+            case STATE_WIN_AI -> g.drawString(MSG_WIN_AI, 70, getHeight() / 2 + 60);
+            default -> throw new RuntimeException("Unchecked gameOverState: " + gameStateType);
         }
+        gameWork = false;
     }
 }
--- src/main/java/ru/gravitana/tic_tac_toe/SettingsWindow.java
+++ src/main/java/ru/gravitana/tic_tac_toe/SettingsWindow.java
@@ -1,25 +1,130 @@
 package ru.gravitana.tic_tac_toe;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SettingsWindow extends JFrame {
-    private static final int WINDOW_WIDTH = 350;
-    private static final int WINDOW_HEIGHT = 230;
+    private static final int WINDOW_WIDTH = 230;
+    private static final int WINDOW_HEIGHT = 350;
 
-    JButton btnStart = new JButton("Start new game");
+    private int size = 3;
+    private int sizeWin = 3;
+    private int choice = 0;
+
+    JButton btnStart;
 
     SettingsWindow(GameWindow gameWindow) {
+        btnStart = new JButton("Start new game");
+
         setLocationRelativeTo(gameWindow);
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
         btnStart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                gameWindow.startNewGame(0, 3, 3, 3);
                 setVisible(false);
+                gameWindow.startNewGame(choice, size, size, sizeWin);
             }
         });
-        add(btnStart);
+
+        JPanel modePanel = createModeComponent();
+        JPanel sizePanel = createSizeComponent();
+        JPanel winLengthPanel = createWinLengthComponent();
+
+        JPanel menu = new JPanel(new GridLayout(3, 1));
+        menu.add(modePanel);
+        menu.add(sizePanel);
+        menu.add(winLengthPanel);
+
+        add(menu);
+        add(btnStart, BorderLayout.SOUTH);
+    }
+
+
+    private JPanel createModeComponent(){
+        JPanel modePanel = new JPanel(new GridLayout(3, 1));
+
+        JLabel message = new JLabel("Выберите режим игры:");
+
+        JRadioButton choice1 = new JRadioButton("Человек против человека");
+        JRadioButton choice2 = new JRadioButton("Человек против AI");
+        choice1.setSelected(true);
+
+        choice1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 0;
+            }
+        });
+
+        choice2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 1;
+            }
+        });
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(choice1);
+        buttonGroup.add(choice2);
+
+        modePanel.add(message);
+        modePanel.add(choice1);
+        modePanel.add(choice2);
+
+        return modePanel;
+    }
+
+    private JPanel createSizeComponent(){
+        JPanel modePanel = new JPanel(new GridLayout(3, 1));
+
+        JLabel message1 = new JLabel("Выберите размеры поля:");
+        JLabel message2 = new JLabel("Установленный размер поля: " + size);
+
+        JSlider sizer = new JSlider(3, 10, size);
+
+        sizer.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                size = sizer.getValue();
+                message2.setText("Установленный размер поля: " + size);
+            }
+        });
+
+        modePanel.add(message1);
+        modePanel.add(message2);
+        modePanel.add(sizer);
+
+        return modePanel;
+    }
+
+    private JPanel createWinLengthComponent(){
+        JPanel modePanel = new JPanel(new GridLayout(3, 1));
+
+        JLabel message1 = new JLabel("Выберите длину для победы:");
+        JLabel message2 = new JLabel("Установленная длина: " + sizeWin);
+
+        JSlider win = new JSlider(3, 10, sizeWin);
+
+        win.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if(win.getValue() > size){
+                    win.setValue(size);
+                }
+                sizeWin = win.getValue();
+                message2.setText("Установленная длина: " + sizeWin);
+            }
+        });
+
+        modePanel.add(message1);
+        modePanel.add(message2);
+        modePanel.add(win);
+
+        return modePanel;
     }
 }
